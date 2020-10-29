92c159b71d8fb115f95cb4f44332108470ebdbff
Frederik Christensen <fchristensen26@gmail.com>
2020-10-29 08:59:23 +0100

    Yes

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace H5Serv.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
